891c1cc5c8769a9d7c84aa35646e7df414decedd
TranHoangDat <tranhoangdat75@gmail.com>
2022-04-25 16:59:55 +0700

    Add access-wallet functionality

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -4,3 +4,6 @@ const url = `${process.env.REACT_APP_API}/auth`;
 
 export const createPassword = password =>
   axios.post(`${url}/createPassword`, { password: password });
+
+export const accessWallet = (keystore, password) =>
+  axios.post(`${url}/accessWallet`, { keystore: keystore, password: password });
--- src/components/dashboard/sidebar/DashboardSidebar.jsx
+++ src/components/dashboard/sidebar/DashboardSidebar.jsx
@@ -5,7 +5,7 @@ import {
   Drawer,
   drawerClasses,
   List,
-  ListItem,
+  ListItemButton,
   ListItemIcon,
   ListItemText,
 } from "@mui/material";
@@ -13,6 +13,9 @@ import AccountCard from "./AccountCard";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import LogoutIcon from "@mui/icons-material/Logout";
+import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const drawerWidth = 300;
 
@@ -66,40 +69,71 @@ const listItemTextStyle = {
 
 const list = [
   {
+    icon: <ArrowCircleUpIcon />,
+    label: "Send",
+    section: "send",
+  },
+  {
     icon: <DashboardIcon />,
-    name: "Dashboard",
+    label: "Dashboard",
+    section: "main",
   },
   {
     icon: <BusinessCenterIcon />,
-    name: "Transaction",
+    label: "Transactions",
+    section: "transactions",
   },
 ];
 
 export default function DashboardSidebar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [currentSection, setCurrentSection] = useState(
+    location.pathname.split("/").pop(),
+  );
+
+  const handleClickListItem = section => {
+    const urlSegments = location.pathname.split("/");
+    const lastSegment = urlSegments.pop();
+
+    if (lastSegment === section) {
+      return;
+    }
+
+    setCurrentSection(section);
+    urlSegments.push(section);
+    navigate(urlSegments.join("/"));
+  };
+
   return (
     <Drawer sx={wrapperStyle} variant="permanent" anchor="left">
       <Box sx={containerStyle}>
         <Box sx={imageWrapperStyle}>
-          <img src="images/home/logo-mew.svg" />
+          <img src="/images/home/logo-mew.svg" />
         </Box>
         <AccountCard />
       </Box>
       <List>
         {list.map(item => (
-          <ListItem sx={listItemStyle} button key={item.name}>
+          <ListItemButton
+            onClick={() => handleClickListItem(item.section)}
+            selected={currentSection === item.section}
+            sx={listItemStyle}
+            key={item.name}
+          >
             <ListItemIcon>{item.icon}</ListItemIcon>
-            <ListItemText sx={listItemTextStyle} primary={item.name} />
-          </ListItem>
+            <ListItemText sx={listItemTextStyle} primary={item.label} />
+          </ListItemButton>
         ))}
       </List>
       <Divider />
       <List>
-        <ListItem sx={listItemStyle} button>
+        <ListItemButton sx={listItemStyle}>
           <ListItemIcon>
             <LogoutIcon />
           </ListItemIcon>
           <ListItemText sx={listItemTextStyle} primary="Logout" />
-        </ListItem>
+        </ListItemButton>
       </List>
     </Drawer>
   );
