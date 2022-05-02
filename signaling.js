99d125aa82cd19018c9a1e141a2d47d45b1af7dd
TranHoangDat <tranhoangdat75@gmail.com>
2022-05-02 12:43:42 +0700

    Add signaling p2p

---
+++ signaling.js
@@ -0,0 +1,17 @@
+const opened = [];
+// "opened" for holding addresses,
+
+export const signalingHandler = (socket, io) => {
+  const startOpenState = address => {
+    socket.broadcast.emit("welcome", address);
+    opened.push(address);
+  };
+
+  const sendSignalHandler = ({ from, to, data }) => {
+    socket.broadcast.to(to).emit("receiveSignal", { from, data });
+  };
+
+  socket.on("open", startOpenState);
+  socket.on("sendSignal", sendSignalHandler);
+  socket.emit("openedSockets", opened);
+};
